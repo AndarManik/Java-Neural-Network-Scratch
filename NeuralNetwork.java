27659da21b3176d2e3299d2c26d5dc7d7f5e0f2a
Andar Manik <59768374+AndarManik@users.noreply.github.com>
2020-02-28 16:40:32 -0500

    Moved the node class into the neuralNetwork class

--- NeuralNetwork.java
+++ NeuralNetwork.java
@@ -1,9 +1,5 @@
-package RecurrentNeuralNetwork;
 import java.io.*;
 import java.util.*;
-
-import RecurrentNeuralNetwork.Node.Activation;
-import RecurrentNeuralNetwork.Node.ActivationDer;
 
 public class NeuralNetwork
 {
@@ -22,6 +18,8 @@ public class NeuralNetwork
       networkFront = new Node[1];
       weightData = new ArrayList<double[]>();
       this.dim = dim;
+      activation = (val) -> (Math.exp(val) - Math.exp(val * -1)) / (Math.exp(val) + Math.exp(val * -1));
+      activationDer = (val) -> 1 - val * val;
       
       construct();
    }
@@ -44,8 +42,10 @@ public class NeuralNetwork
 	   networkFront = new Node[1];
 	   weightData = new ArrayList<double[]>();
 	   dim = new int[(int) Math.log(Math.max(input, output)) + 3];
+	   activation = (val) -> (Math.exp(val) - Math.exp(val * -1)) / (Math.exp(val) + Math.exp(val * -1));
+	      activationDer = (val) -> 1 - val * val;
 		
-	   for(int i = 1; i < dim.length; i++)
+	   for(int i = 0; i < dim.length; i++)
 		   dim[i] = input * (1 - i / (dim.length - 1)) + output * i / (dim.length - 1);
 	   
 	   construct();
@@ -60,7 +60,7 @@ public class NeuralNetwork
 	   activation = activate;
 	   activationDer = activateDer;
 	   
-	   for(int i = 1; i < dim.length; i++)
+	   for(int i = 0; i < dim.length; i++)
 		   dim[i] = input * (1 - i / (dim.length - 1)) + output * i / (dim.length - 1);
 	   
 	   construct();
@@ -73,6 +73,8 @@ public class NeuralNetwork
 		   derCounter = 0;
 		   networkFront = new Node[1];
 		   weightData = new ArrayList<double[]>();
+		   activation = (val) -> (Math.exp(val) - Math.exp(val * -1)) / (Math.exp(val) + Math.exp(val * -1));
+		   activationDer = (val) -> 1 - val * val;
 		      
 		   File file = new File(fileName);
 		   Scanner scanner = new Scanner(file);
@@ -289,4 +291,179 @@ public class NeuralNetwork
 		   return e.toString();
 	   }
    }
+   
+   public class Node
+   {
+   	private Node[] prevLayer;
+   	   
+   	private double val;
+   	private boolean isVal;
+   	   
+   	private double der;
+   	private boolean isDer;
+   	   
+   	private double[] weight;//last weight is bias
+   	private double[] derWeight;
+   	   
+   	private Activation activation;
+   	private ActivationDer activationDer;
+      
+      public Node(Node[] inLayer)//set values for the previous layer and set random weights
+      {
+         prevLayer = inLayer;
+         weight = new double[prevLayer.length + 1];
+         derWeight = new double[prevLayer.length + 1];
+         
+         for(int i = 0; i < weight.length; i++)
+            weight[i] = Math.random() * 2 - 1;
+            
+         weight[weight.length - 1] = Math.random();
+      }
+      
+      public Node(Node[] inLayer, Activation activate, ActivationDer activateDer)//set values for the previous  layer and set random weights
+      {
+         prevLayer = inLayer;
+         weight = new double[prevLayer.length + 1];
+         derWeight = new double[prevLayer.length + 1];
+         
+         for(int i = 0; i < weight.length; i++)
+            weight[i] = Math.random() * 2 - 1;
+            
+         weight[weight.length - 1] = Math.random();
+         
+         activation = activate;
+         activationDer = activateDer;
+      }
+      
+      public double getVal()
+      {
+         if(isVal)
+            return val;
+            
+         double sum = 0;
+         
+         for(int i = 0; i < prevLayer.length; i++)
+            sum += prevLayer[i].getVal() * weight[i];
+         
+         val = activation.activate(sum + weight[weight.length -1]);
+         isVal = true;
+         
+         return val;
+      }
+      
+      public void setVal(double input)
+      {
+         val = input;
+         isVal = true;
+      }
+      
+      public Node[] getPrevLayer()
+      {
+         return prevLayer;
+      }
+      
+      public double getDer() 
+      {
+   	   return der;   
+      }
+      
+      public void setDer(double der)
+      {
+   	  this.der = der;
+         
+         for(int i = 0; i < prevLayer.length; i++)
+            derWeight[i] += prevLayer[i].getVal() * activationDer.activateDer(val) * der;
+            
+         derWeight[derWeight.length - 1] += activationDer.activateDer(val) * der;
+         
+         isDer = true;
+      }
+      
+      public double[] getWeight()
+      {
+         return weight;
+      }
+      
+      public double[] getDerWeight()
+      {
+         return derWeight;
+      }
+      
+      public void saveWeightData(List<double[]> weightData, boolean recur, Node[] networkFront)
+      {
+   	   if(recur && !prevLayer[0].prevLayer.equals(networkFront))
+   	   {
+   		   prevLayer[0].saveWeightData(weightData, true, networkFront);
+   		   
+   		   for(int i = 1; i < prevLayer.length; i++)
+   			   prevLayer[i].saveWeightData(weightData, false, networkFront);
+   	   }
+   	   
+   	   weightData.add(weight);
+      }
+      
+      //assumes prevLayers weights have been calculated. Calculates the derivatives for the nodes which are two layers behind it.
+      // in doing so sets up the assumption for backProp on the next layer up. Stops recursion when the layer two layers behind it is the front of the network
+      public void backProp(Node[] networkFront)
+      {  
+         Node[] twoLayerBack = prevLayer[0].getPrevLayer();
+         
+         if(twoLayerBack[0].getPrevLayer().equals(networkFront))
+            return;
+         
+         for(int i = 0; i < twoLayerBack.length; i++)
+         {
+            double sumDer = 0;
+            
+            for(int j = 0; j < prevLayer.length; j++)//sum the derivatives of node
+           		 sumDer += prevLayer[j].activationDer.activateDer(val) * prevLayer[j].getDer() * prevLayer[j].getWeight()[i];
+              
+            twoLayerBack[i].setDer(sumDer);
+         }
+         
+         prevLayer[0].backProp(networkFront);//recur on layer down
+      }
+      
+      public void updateWeight(int derCounter, double rate)
+      {
+         if(isDer == false)
+            return;
+            
+         for(int i = 0; i < prevLayer.length; i++)
+         {
+            weight[i] -= derWeight[i] * rate / derCounter;
+            prevLayer[i].updateWeight(derCounter, rate);
+            derWeight[i] = 0;
+         }
+         
+         weight[weight.length - 1] -= derWeight[derWeight.length - 1] * rate / derCounter;
+         derWeight[derWeight.length - 1] = 0;
+         isDer = false;
+      }
+      
+      public void clear(Node[] networkFront)
+      {
+         if(!isVal)
+            return;
+         
+         val = 0;
+         isVal = false;
+         
+         if(!prevLayer.equals(networkFront))
+         {
+            for(int i = 0; i < prevLayer.length; i++)
+               prevLayer[i].clear(networkFront);
+         }
+      }
+   }
+   interface Activation
+   {
+	   double activate(double val);
+   }
+   
+   interface ActivationDer
+   {
+	   double activateDer(double val);
+   }
 }
+
